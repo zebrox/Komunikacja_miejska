e1785d19e29aab10e8665f994e32fb9e78056cb6
Zebrox <zebrox@wp.pl>
2017-11-20 14:57:26 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+        entry: "./js/zadanie02.jsx",
+        output: {filename: "./js/out.js" },
+            watch: true,
+            module: {loaders: [{
+                     test: /\.jsx$/,
+                     exclude: /node_modules/,
+                     loader: 'babel-loader',
+                     query: {presets: ['es2015','stage-2','react']}
+                    }]
+            }
+        }
